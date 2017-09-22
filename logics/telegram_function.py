fd0bf4d9833df13a21cd13bc3a8530adeac68c74
RedAlevin <drop@btotd.ru>
2017-09-22 14:18:58 +0300

    v1.1.2 - update telegram function

--- logics/telegram_function.py
+++ logics/telegram_function.py
@@ -2,13 +2,12 @@ import files.constant as const
 import logics.search_image
 import os
 import logics.blur
-import telebot
 
 
 def if_my_message(message):
     message_id = message.from_user.id
     message_text = message.text
-    """
+
     if message_id == const.admin_id:
         if message_text == "+":
             pass
@@ -16,10 +15,6 @@ def if_my_message(message):
             pass
     else:
         pass
-    """
-    message_text = logics.search_image.ru_to_search(message_text)
-    generate_img(message_text)
-
 
 
 def generate_img(name):
@@ -29,12 +24,3 @@ def generate_img(name):
         logics.blur.photo("files/saves/{}".format(dir[i]), "files/blur/{}.jpg".format(i))
         os.remove("files/saves/{}".format(dir[i]))
     print("Complited")
-
-
-def push_img(bot):
-    dir = os.listdir(os.getcwd() + "/files/blur")
-    for i in range(len(dir)):
-        with open("files/blur/{}".format(dir[i]), "rb") as img:
-            bot.send_chat_action(const.admin_id, "upload_photo")
-            bot.send_photo(const.admin_id, img)
-            os.remove("files/blur/{}".format(dir[i]))
